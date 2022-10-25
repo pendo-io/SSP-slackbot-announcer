b35509de07f457a710234f538e0da83ceca80638
Joseph Wilbur <joseph@pendo.io>
2022-10-25 10:55:18 -0400

    fresh start

--- dist/modal-profile/profile.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*------------------
-   PROFILE TYPES
-------------------*/
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-;
-;
-//# sourceMappingURL=profile.interface.js.map
